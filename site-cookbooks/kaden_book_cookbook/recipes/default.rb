953aa43d4ce0428d322c3d0145607cddcf7e938f
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-02-10 07:05:45 +0900

    passenger

--- site-cookbooks/kaden_book_cookbook/recipes/default.rb
+++ site-cookbooks/kaden_book_cookbook/recipes/default.rb
@@ -19,3 +19,13 @@ end
 rbenv_gem "bundler" do
   ruby_version "2.2.4"
 end
+
+include_recipe "passenger_apache2"
+
+web_app "myproj" do
+  cookbook "passenger_apache2"
+  docroot "/opt/kadenpotal/public"
+  server_name "myproj.#{node[:domain]}"
+  server_aliases [ "myproj", node[:hostname] ]
+  rails_env "production"
+end
